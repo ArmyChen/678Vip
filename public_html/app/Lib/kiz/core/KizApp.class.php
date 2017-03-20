3691cdc842c7957a8086e8ce59bc4c668eb29ad7
chenarmy.cool@163.com <chenjun111>
2017-03-20 17:23:33 +0800

    kiz 初始化

---
+++ public_html/app/Lib/kiz/core/KizApp.class.php
@@ -0,0 +1,55 @@
+<?php
+// +----------------------------------------------------------------------
+// | Fanweo2o商业系统 最新版V3.03.3285  含4个手机APP。
+// +----------------------------------------------------------------------
+// | 购买本程序，请联系QQ：78282385  旺旺名：alert988
+// +----------------------------------------------------------------------
+// | 淘宝购买地址：https://shop36624490.taobao.com/
+// +----------------------------------------------------------------------
+require APP_ROOT_PATH.'app/Lib/kiz/core/KizBaseModule.class.php';
+require APP_ROOT_PATH.'app/Lib/kiz/core/kiz_init.php';
+define("CTL",'ctl');
+define("ACT",'act');
+
+class KizApp{
+	private $module_obj;
+	//网站项目构造
+	public function __construct(){
+		if($GLOBALS['pay_req'][CTL])
+			$_REQUEST[CTL] = $GLOBALS['pay_req'][CTL];
+		if($GLOBALS['pay_req'][ACT])
+			$_REQUEST[ACT] = $GLOBALS['pay_req'][ACT];
+		
+		$module = strtolower($_REQUEST[CTL]?$_REQUEST[CTL]:"index");
+		$action = strtolower($_REQUEST[ACT]?$_REQUEST[ACT]:"index");
+		
+		$module = filter_ctl_act_req($module);
+		$action = filter_ctl_act_req($action);
+		if(!file_exists(APP_ROOT_PATH."app/Lib/kiz/".$module."Module.class.php"))
+		$module = "index";
+
+
+		require_once APP_ROOT_PATH."app/Lib/kiz/".$module."Module.class.php";
+
+		if(!class_exists($module."Module"))
+		{
+			$module = "index";
+			require_once APP_ROOT_PATH."app/Lib/kiz/".$module."Module.class.php";
+		}
+		if(!method_exists($module."Module",$action))
+		$action = "index";
+		
+		define("MODULE_NAME",$module);
+		define("ACTION_NAME",$action);
+		
+		$module_name = $module."Module";
+		$this->module_obj = new $module_name;
+		$this->module_obj->$action();
+	}
+	
+	public function __destruct()
+	{
+		unset($this);
+	}
+}
+?>
