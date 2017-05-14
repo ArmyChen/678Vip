adac10bebb29c180849d7f253fb26237b441f174
chenarmy.cool@163.com <chenjun111>
2017-05-14 09:05:22 +0800

    出库删除bug

--- public_html/app/Lib/kiz/ajaxModule.class.php
+++ public_html/app/Lib/kiz/ajaxModule.class.php
@@ -4365,18 +4365,8 @@ class ajaxModule extends KizBaseModule{
                 $res=$GLOBALS['db']->autoExecute(DB_PREFIX."cangku_menu_gys", $data_gys ,"INSERT");
             }else{ //出库
                 $check=$GLOBALS['db']->getRow("select mstock from fanwe_cangku_menu ".$sqlstr);
-                if($order_num>$check['mstock']){
-                    $return['flag'] = null;
-                    $return['exception'] = null;
-                    $return['refresh'] = false;
-                    $return['success'] = false;
-                    $return['message'] ="库存不足,非法提交！";
-                    echo json_encode($return);exit;
+                $res=$GLOBALS['db']->query("update ".DB_PREFIX."cangku_menu set mstock=mstock-$order_num,ctime='".to_date(NOW_TIME)."' ".$sqlstr);
 
-//                    showBizErr("库存不足,非法提交，后果自负！",0,url("biz","cangku#index&id=$slid"));
-                }else{//操作减库存
-                    $res=$GLOBALS['db']->query("update ".DB_PREFIX."cangku_menu set mstock=mstock-$order_num,ctime='".to_date(NOW_TIME)."' ".$sqlstr);
-                }
             }
 
             //
